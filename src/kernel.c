02be4210883525a06482da6f9ffc66bc728bb566
Der Bejijing <62348565+DerBejijing@users.noreply.github.com>
2021-11-21 22:22:25 +0100

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -4,12 +4,17 @@
 #include "../include/string.h"
 #include "../include/pios.h"
 
+// kernel entrypoint
 void kmain() {
 
+	// clears the screen
 	clear_screen();
+	
+	// sets the default colors for printing text
 	set_default_color(COLOR_BLACK_LIGHT_GREEN);
 	set_default_selection_color(COLOR_LIGHT_GREEN_BLACK);
 
+	// renders the menues
 	menu_management();
 
 }
