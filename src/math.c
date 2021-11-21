d127e195f5fbf291f5e84d361822b9785c0bea04
Der Bejijing <62348565+DerBejijing@users.noreply.github.com>
2021-11-21 22:43:43 +0100

    Update math.c

--- src/math.c
+++ src/math.c
@@ -1,17 +1,33 @@
 #include "../include/math.h"
 
+// parse a string to a 32bit signed integer
+// accepts a leading dash to indicate negative numbers
 int32 string_to_int32(string str) {
 
+	// when iterating backwards through the string, multiplier gets multiplied by ten
+	//  on every iteration
+	// this is how our decimal-system works
 	int32 multiplier = 1;
+	
+	// stores the resulting integer
 	int32 result = 0;
+	
+	// when iterating, this stores the value of the current character
 	int16 add = 0;
 
+	// iterate backwards
 	for(int16 pos = str_len(str)-1; pos >= 0; --pos) {
+		// check if the resulting number will be positive
 		if(str[pos] != '-') {
+			// convert character to an int and subtract 48
+			// that way the ascii-value 48 refers to 0, 49 to 1 and so on
 			add = str[pos] - 48;
+			
+			// append the value to the result and multiply with the multiplier to simulate a shift
 			result += add * multiplier;
 			multiplier *= 10;
 		} else {
+			// multiply result with -1
 			return result * -1;
 		}
 	}
@@ -46,12 +62,15 @@ string uint64_to_string(uint64 num) {
 }
 
 
+// this is where pi will be calculated using the Gregory-Leibniz series
 void calculate_pi_gl(uint8 visualOutput) {
 
 	print("\nStill to be developed (README.md) :(\n");
 
 }
 
+
+// this is where pi will be calculated using Nilakantha series
 void calculate_pi_n(uint8 visualOutput) {
 
 	print("\nStill to be developed (README.md) :(\n");
